3549011ca35f5aaa467eb3e2faedac095a21ff93
sozysozbot <sozysozbot@gmail.com>
2023-05-18 06:30:53 +0900

    12 ページ目、あとコンバータ

---
+++ tmp/convert.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const tsv = fs.readFileSync("tmp.tsv", { encoding: "utf-8" });
+const ans = tsv.split("\n").map(row => {
+    const [pmcp, ja] = row.split("\t");
+    return JSON.stringify({ "x-pmcp": pmcp, ja });
+}).join(",\n")
+fs.writeFileSync("tmp_converted.txt", ans);
