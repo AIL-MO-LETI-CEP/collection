d6fb88909f1e65e2d299a44df4e7d76ddf812dca
sozysozbot / hsjoihs <sozysozbot@gmail.com>
2023-05-18 07:07:03 +0900

    8 ページ目完了

--- tmp/convert.js
+++ tmp/convert.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const tsv = fs.readFileSync("tmp.tsv", { encoding: "utf-8" });
-const ans = tsv.split("\n").map(row => {
+const ans = tsv.split(/\r\n|\n/).map(row => {
     const [pmcp, ja] = row.split("\t");
     return JSON.stringify({ "x-pmcp": pmcp, ja });
 }).join(",\n")
