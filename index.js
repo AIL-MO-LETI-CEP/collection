d649d0408afcc05cec36a05c467bbcec1187e7e6
sozysozbot <sozysozbot@gmail.com>
2023-08-26 10:19:02 +0900

    Set up TypeScript and JSON module

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const ___________json_1 = __importDefault(require("./\u6211\u3005\u4E4B\u904A\u8853(\u8A00\u5C06\u8A00).json"));
+console.log(___________json_1.default);
---
+++ index.ts
@@ -0,0 +1,3 @@
+import mi_cet_leti_belpic from './我々之遊術(言将言).json';
+
+console.log(mi_cet_leti_belpic);
